b333a41d1c66812efa84701db8b7de53ecb235ea
Gawah Chan <gawahchan@gmail.com>
2019-10-22 13:05:08 +0100

    Added NavigationBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavigationBar from './components/navigationbar/navigationbar.ui';
 import HomePage from './components/homepage/homepage.ui';
 import About from './components/about/about.ui';
 
 const App: React.FC = () => {
   return (
     <Router>
+      <NavigationBar />
       <Switch>
         <Route exact path="/">
           <HomePage />
