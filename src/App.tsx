4577a36afbddcd0614c0a3f936cde7e9feb1db6a
Gawah Chan <gawahchan@gmail.com>
2019-10-22 11:30:43 +0100

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/homepage/homepage.ui';
+import About from './components/about/about.ui';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
